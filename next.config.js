25bfe354042549fc7b8b4f1c9dcee15b64d59aac
keith-carr <justkeithcarr@gmail.com>
2021-09-27 18:36:22 -0400

    added env to next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   env: {
-    NEXT_CONTENTFUL_SPACE_ID: 'dx4qvrmnbbvs',
-    NEXT_CONTENTFUL_ACCESS_TOKEN: 'OM_ZIoz2uKmf5Uqd-Szux7PPk6wcFp-38KvbCWVTJs8',
-    NEXT_CONTENTFUL_PERSONAL_ACCESS_TOKEN: 'CFPAT-IJL_ec7JQ6DmB36VRIwLwL9NfqqR0S6qWvTp_5WkcH4'
+    NEXT_CONTENTFUL_SPACE_ID: "dx4qvrmnbbvs",
+    NEXT_CONTENTFUL_ACCESS_TOKEN: "OM_ZIoz2uKmf5Uqd-Szux7PPk6wcFp-38KvbCWVTJs8",
+    NEXT_CONTENTFUL_PERSONAL_ACCESS_TOKEN:
+      "CFPAT-IJL_ec7JQ6DmB36VRIwLwL9NfqqR0S6qWvTp_5WkcH4",
+    NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyBfGfgF87NH-eNgrZS7SRQWsv8QU53gzrs",
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: "property-listings-4ec09",
   },
   images: {
-    domains: ['images.ctfassets.net'],
+    domains: ["images.ctfassets.net"],
   },
-}
+};
